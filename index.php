5ff552e91056ef4aee3ccf5001171d9b42c1af9e
komal800 <komalmaru2411@gmail.com>
2023-08-28 18:22:59 +0530

    try 2

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 echo "hello" ;
 ?>
+
+git init -- to initialize
+
+git status -- tp know the status of filesize
+
+git add . -- to add all files 
+
+
+--- (after setting user.name and user.email)
+git commit -m "message"  
+
+U-- untracked
+A -- added
+m -- modified
+
