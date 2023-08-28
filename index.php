ff1675695bfcbb225c861e3181509a8f8607ed50
komal800 <komalmaru2411@gmail.com>
2023-08-28 18:33:40 +0530

    new new

--- index.php
+++ index.php
@@ -4,13 +4,18 @@ echo "hello" ;
 
 git init -- to initialize
 
-git status -- tp know the status of filesize
+git status -- t know the status of files
 
 git add . -- to add all files 
 
+git config --global/local user.name "username"  // optional
+git config --global/local user.email "email"    // optional
 
 --- (after setting user.name and user.email)
 git commit -m "message"  
+
+and then the 3 command after commit as stated in github
+
 
 U-- untracked
 A -- added
